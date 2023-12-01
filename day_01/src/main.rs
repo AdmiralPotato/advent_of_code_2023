e144c60ab7c0d2480adf540e4ed9ff1258f12f41
Admiral Potato <admiral@nuclearpixel.com>
2023-12-01 01:05:47 -0700

    day_01: post-solution improvements

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -3,38 +3,29 @@ use std::str::Split;
 
 fn parse_line_part_1(text: &str) -> u8 {
     let mut result = String::new();
-    let mut last_char = ' ';
+    let mut last_char = None;
     let mut count = 0;
     for char in text.chars() {
-        let is_digit = matches!(
-            char,
-            '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
-        );
-        if is_digit {
-            last_char = char;
+        if char.is_ascii_digit() {
+            last_char = Some(char);
             count += 1;
             if count == 1 {
-                result += &format!("{last_char}");
+                result.push(char);
             }
         }
     }
     if result.len() == 1 {
-        result += &format!("{last_char}");
+        result.push(last_char.unwrap());
     }
     let oof = format!("What is combined? {result}");
     (*result).parse::<u8>().expect(&oof)
 }
+
 fn part_1(lines: &Split<&str>) -> u32 {
-    let mut total = 0;
-    for line in lines.clone() {
-        if line.is_empty() {
-            continue;
-        }
-        let parsed = parse_line_part_1(line) as u32;
-        // println!("Input: {line}, parsed: {parsed}");
-        total += parsed;
-    }
-    total
+    lines
+        .clone()
+        .map(|line| parse_line_part_1(line) as u32)
+        .sum()
 }
 
 const STRINGS_TO_NUMBERS: [(&str, char); 10] = [
@@ -53,11 +44,8 @@ fn parse_line_part_2(text: &str) -> u8 {
     let mut result = String::new();
     let mut last_char = ' ';
     let mut count = 0;
-    for (char_index, mut char) in text.chars().enumerate() {
-        let mut is_digit = matches!(
-            char,
-            '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
-        );
+    for (char_index, mut char) in text.char_indices() {
+        let mut is_digit = char.is_ascii_digit();
         if !is_digit {
             for (word, char_value) in STRINGS_TO_NUMBERS {
                 if text[char_index..].starts_with(word) {
@@ -80,9 +68,9 @@ fn parse_line_part_2(text: &str) -> u8 {
     let oof = format!("What is combined? {result}");
     (*result).parse::<u8>().expect(&oof)
 }
-fn part_2(lines: &Split<&str>) -> u32 {
+fn part_2<'a>(lines: impl Iterator<Item = &'a str>) -> u32 {
     let mut total = 0;
-    for line in lines.clone() {
+    for line in lines {
         if line.is_empty() {
             continue;
         }
@@ -95,16 +83,51 @@ fn part_2(lines: &Split<&str>) -> u32 {
 
 fn main() {
     let text_file_string = read_to_string("./day_01/input.txt").expect("It was supposed to work");
-    let sample_input = text_file_string.split("\n");
+    let sample_input = text_file_string.trim().split("\n");
     let part_1_result = part_1(&sample_input);
     println!("WHAT IS part_1_result?? {part_1_result}");
-    let part_2_result = part_2(&sample_input);
+    let part_2_result = part_2(sample_input.clone());
     println!("WHAT IS part_2_result?? {part_2_result}");
+    /*
+       WHAT IS part_1_result?? 56049
+       WHAT IS part_2_result?? 54530
+    */
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const SAMPLE_A: &[(&str, u8)] = &[
+        ("1abc2", 12),
+        ("pqr3stu8vwx", 38),
+        ("a1b2c3d4e5f", 15),
+        ("treb7uchet", 77),
+    ];
+    const SAMPLE_A_RESULT: u32 = 142;
+    #[test]
+    fn part_1_full_test() {
+        let mut ok = true;
+        let total: u32 = SAMPLE_A
+            .iter()
+            .map(|(input, output)| {
+                let calculated_output = parse_line_part_1(input);
+                println!("{input:?} got {calculated_output}, expected {output}");
+                if calculated_output != *output {
+                    println!("^^^ WRONG! ^^^");
+                    ok = false;
+                }
+                calculated_output as u32
+            })
+            .sum();
+        println!("Result was {total}, expected {SAMPLE_A_RESULT}");
+        if total != SAMPLE_A_RESULT {
+            println!("^^^ WRONG! ^^^");
+            ok = false;
+        }
+        assert!(ok);
+    }
+
     const SAMPLE_INPUT_A: &[&str] = &["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
     const SAMPLE_INPUT_B: &[&str] = &[
         "two1nine",
@@ -144,7 +167,7 @@ mod tests {
     fn part_2_test() {
         let text_file_string = read_to_string("./input_sample_2.txt").unwrap();
         let sample_input = text_file_string.trim().split("\n");
-        let part_2_result = part_2(&sample_input);
+        let part_2_result = part_2(sample_input);
         assert_eq!(part_2_result, 281);
     }
     #[test]
