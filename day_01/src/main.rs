df84bf7448db0609c51ce13514d7bd74710e4f25
Admiral Potato <admiral@nuclearpixel.com>
2023-11-30 23:10:26 -0700

    day_01: pass part 1, I have regrets

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,4 +1,6 @@
 use std::fs::read_to_string;
+use std::str::Split;
+
 fn parse_line_part_1(text: &str) -> u8 {
     let mut result = String::new();
     let mut last_char = ' ';
@@ -19,22 +21,44 @@ fn parse_line_part_1(text: &str) -> u8 {
     if result.len() == 1 {
         result += &format!("{last_char}");
     }
-    (*result).parse::<u8>().expect("cannot parse u8")
+    let oof = format!("What is combined? {result}");
+    (*result).parse::<u8>().expect(&oof)
+}
+fn part_1(lines: &Split<&str>) -> u32 {
+    let mut total = 0;
+    for line in lines.clone() {
+        if line.is_empty() {
+            continue;
+        }
+        let parsed = parse_line_part_1(line) as u32;
+        println!("Input: {line}, parsed: {parsed}");
+        total += parsed;
+    }
+    total
 }
 
 fn main() {
-    let sample_input = read_to_string("./day_01/input.txt").expect("It was supposed to work");
+    let text_file_string = read_to_string("./day_01/input.txt").expect("It was supposed to work");
+    let sample_input = text_file_string.split("\n");
+    let part_1_result = part_1(&sample_input);
     // let result = parse_line(4);
     // println!("WHAT IS result?? {result}");
-    println!("WHAT IS sample_input?? {sample_input}");
+    println!("WHAT IS part_1_result?? {part_1_result}");
 }
-
-const SAMPLE_INPUT: &[&str] = &["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    const SAMPLE_INPUT: &[&str] = &["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
 
+    #[test]
+    fn part_1_test() {
+        // Why are the test run in a different folder? ¯\_(ツ)_/¯
+        let text_file_string = read_to_string("./input_sample.txt").unwrap();
+        let sample_input = text_file_string.trim().split("\n");
+        let part_1_result = part_1(&sample_input);
+        assert_eq!(part_1_result, 142);
+    }
     #[test]
     fn test_0() {
         assert_eq!(parse_line_part_1(SAMPLE_INPUT[0]), 12);
