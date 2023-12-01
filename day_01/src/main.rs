242730a2e4cab0f6e1db5bad931583a858519fae
Admiral Potato <admiral@nuclearpixel.com>
2023-11-30 21:59:49 -0700

    day_01: read text first

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,10 +1,15 @@
-fn yolo (a: u8) -> u8 {
+use std::fs::read_to_string;
+
+fn yolo(a: u8) -> u8 {
     a
 }
 
 fn main() {
+    let sample_input =
+        read_to_string("./day_01/input_sample.txt").expect("It was supposed to work");
     let result = yolo(4);
-    println!("WHAT IS NUMBER?? {result}");
+    println!("WHAT IS result?? {result}");
+    println!("WHAT IS sample_input?? {sample_input}");
 }
 
 #[cfg(test)]
