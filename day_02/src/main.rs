3d81cd3f0c97550f2f2c68ee1238df07328000f8
Admiral Potato <admiral@nuclearpixel.com>
2023-12-02 01:14:59 -0700

    day_02: part 2 cleanup

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -12,13 +12,10 @@ impl Cubes {
     fn from_str(text: &str) -> Cubes {
         let mut result = Cubes::default();
         for color_fragment in text.trim().split(", ") {
-            let mut color_fragment = color_fragment.split(" ");
-            let count: u8 = color_fragment
-                .next()
-                .unwrap()
-                .parse()
-                .expect("unable to parse ");
-            let color = color_fragment.next().unwrap();
+            let color_fragment = color_fragment.split_once(' ').unwrap();
+            let count: u8 =
+                color_fragment.0.parse().expect("unable to parse ");
+            let color = color_fragment.1;
             match color {
                 "red" => result.red = count,
                 "green" => result.green = count,
@@ -29,7 +26,9 @@ impl Cubes {
         result
     }
     fn test(&self, limit: Cubes) -> bool {
-        self.red <= limit.red && self.green <= limit.green && self.blue <= limit.blue
+        self.red <= limit.red
+            && self.green <= limit.green
+            && self.blue <= limit.blue
     }
     fn expand(&mut self, bounds: &Cubes) {
         self.red = cmp::max(self.red, bounds.red);
@@ -51,20 +50,15 @@ const PART_1_CONFIG: Cubes = Cubes {
 };
 
 fn parse_line(text: &str) -> ParsedLine {
-    let mut splits = text.split(": ");
-    let mut id_fragment = splits.next().unwrap().split(" ");
-    id_fragment.next();
-    let id = id_fragment
-        .next()
+    let splits = text.split_once(": ").unwrap();
+    let id = splits
+        .0
+        .split_once(' ')
         .unwrap()
+        .1
         .parse()
         .expect("Unable to parse ID");
-    let handfulls = splits
-        .next()
-        .unwrap()
-        .split("; ")
-        .map(Cubes::from_str)
-        .collect();
+    let handfulls = splits.1.split("; ").map(Cubes::from_str).collect();
     ParsedLine { id, handfulls }
 }
 fn test_line_part_1(text: &str) -> u8 {
@@ -99,23 +93,21 @@ fn test_line_part_2(text: &str) -> u32 {
     total
 }
 
-fn part_1(lines: &Split<&str>) -> u32 {
+fn part_1(lines: &Split<char>) -> u32 {
     lines
         .clone()
         .map(|line| test_line_part_1(line) as u32)
         .sum()
 }
 
-fn part_2(lines: &Split<&str>) -> u32 {
-    lines
-        .clone()
-        .map(|line| test_line_part_2(line) as u32)
-        .sum()
+fn part_2(lines: &Split<char>) -> u32 {
+    lines.clone().map(test_line_part_2).sum()
 }
 
 fn main() {
-    let text_file_string = read_to_string("./day_02/input.txt").expect("It was supposed to work");
-    let sample_input = text_file_string.trim().split("\n");
+    let text_file_string =
+        read_to_string("./day_02/input.txt").expect("It was supposed to work");
+    let sample_input = text_file_string.trim().split('\n');
     let part_1_result = part_1(&sample_input);
     println!("WHAT IS part_1_result?? {part_1_result}");
     let part_2_result = part_2(&sample_input);
@@ -134,7 +126,7 @@ mod tests {
     fn part_1_test() {
         // Why are the test run in a different folder? ¯\_(ツ)_/¯
         let text_file_string = read_to_string("./input_sample.txt").unwrap();
-        let sample_input = text_file_string.trim().split("\n");
+        let sample_input = text_file_string.trim().split('\n');
         let part_1_result = part_1(&sample_input);
         assert_eq!(part_1_result, 8);
     }
@@ -168,7 +160,9 @@ mod tests {
     #[test]
     fn part_1_test_pass() {
         assert_eq!(
-            test_line_part_1("Game 42: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
+            test_line_part_1(
+                "Game 42: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+            ),
             42
         );
     }
@@ -199,14 +193,16 @@ mod tests {
     #[test]
     fn part_2_test_0() {
         assert_eq!(
-            test_line_part_2("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
+            test_line_part_2(
+                "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+            ),
             48
         );
     }
     #[test]
     fn part_2_test() {
         let text_file_string = read_to_string("./input_sample.txt").unwrap();
-        let sample_input = text_file_string.trim().split("\n");
+        let sample_input = text_file_string.trim().split('\n');
         assert_eq!(part_2(&sample_input), 2286);
     }
 }
